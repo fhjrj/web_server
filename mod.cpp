2bf6ef7007d0eeba9f270ad658331838579b35c6
fhjrj <166215873+fhjrj@users.noreply.github.com>
2024-09-08 11:55:42 +0800

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -9,12 +9,6 @@ MOD::MOD(){
     //触发组合模式,默认listenfd LT + connfd LT
     TRIGMode = 0;
 
-    //listenfd触发模式，默认LT
-    LISTENTrigmode = 0;
-
-    //connfd触发模式，默认LT
-    CONNTrigmode = 0;
-
     //优雅关闭链接，
     OPT_LINGER = 0;
 
