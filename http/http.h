8a8c41ff910f6ef666bdc9525701b9bd9c68d8ce
fhjrj <166215873+fhjrj@users.noreply.github.com>
2024-09-12 16:32:49 +0800

    Update http.h

--- http/http.h
+++ http/http.h
@@ -30,7 +30,7 @@ class http_conn
 public:
     static const int FILENAME_LEN = 200;
     static const int READ_BUFFER_SIZE = 2048;
-   static const int WRITE_BUFFER_SIZE = 2048*3;
+   static const int WRITE_BUFFER_SIZE = 2048*4;
     enum METHOD
     {
         GET = 0,
